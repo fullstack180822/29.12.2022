cdd625e35bccdb399534f4aa2c20be132a83c273
micserv010821 <dotnet290320@gmail.com>
2022-12-29 18:52:29 +0200

    upload

---
+++ app.js
@@ -0,0 +1,23 @@
+// index.js
+const express = require('express')
+const router = express.Router()
+const path = require('path')
+const url = require ('url')
+const cors = require ('cors')
+const { response } = require('express')
+
+const port = 8080;
+
+const app = express()
+
+app.use(express.static(path.join('.','/static/'))) // /static/index.html
+// page1.html
+
+app.get('/fruit', (req, resp) => {
+    resp.writeHead(201);
+    resp.end('Banan is my favorite fruit!')
+})
+
+app.listen(port, () => {
+    console.log(`Listening to port ${port}`);
+})
